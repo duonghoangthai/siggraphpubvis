ea2ab4bfb8365e9047a130a708a1ad3cacc030a6
hoangthaiduong <hoangthaiduong@gmail.com>
2015-11-28 19:38:59 -0700

    Show the keyword now.

--- KeywordVis.js
+++ KeywordVis.js
@@ -11,19 +11,33 @@ function KeywordVis(keywordGraph) {
 }
 
 KeywordVis.prototype.update = function() {
-    var border_x = 15;
-    var border_y = 15;
-    var squares = this.svg.append("g")
-        .attr("transform", "translate(" + border_x + "," + border_y + ")")
-        .selectAll("rect").data(this.keywordGraph.vertices);
-
+    var border_x = 25;
+    var border_y = 25;
     var text_width = 50;
-    var text_height = 30;
-    squares.enter().append("rect")
-        .attr("x", function(d, i) { return (i % 21) * text_width; })
-        .attr("y", function(d, i) { return Math.floor(i / 21) * text_height; })
+    var text_height = 25;
+
+    var cells = this.svg
+        .append("g")
+        .attr("transform", "translate(" + border_x + "," + border_y + ")")
+        .selectAll("g")
+        .data(this.keywordGraph.vertices)
+        .enter().append("g")
+        .attr("transform", function(d, i) {
+            return "translate(" + (i % 23) * text_width + ","
+                              + Math.floor(i / 23) * text_height + ")"});
+
+    cells.append("rect")
         .attr("width", text_width)
         .attr("height", text_height)
-        .style("stroke", "white")
-        .style("fill", "rgb(119, 119, 119)");
+        .style("stroke", "rgb(248, 248, 248)")
+        .style("stroke-width", "10")
+        //.style("fill", "rgb(119, 119, 119)");
+        .style("fill", "rgb(248, 248, 248)")
+
+    cells.append("text")
+        .attr("x", 5)
+        .attr("y", function(d) { return text_height / 3 * 2; })
+        .attr("fill", "rgb(20, 20, 20)")
+        .attr("font-size", "20px")
+        .text(function(d) { return d.text; });
 }
