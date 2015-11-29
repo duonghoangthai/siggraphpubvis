c649dda6040e91b2ab26e3b0efa2362fcad4a3e0
hoangthaiduong <hoangthaiduong@gmail.com>
2015-11-29 00:19:05 -0700

    Add transition.
    Change the mouse cursor to pointer when hovering above the keywords.

--- KeywordVis.js
+++ KeywordVis.js
@@ -32,12 +32,13 @@ KeywordVis.prototype.init = function() {
     cells.append("rect")
         .attr("width", text_width)
         .attr("height", text_height)
-        .style("stroke", "rgb(248, 248, 248)")
-        .style("stroke-width", "8")
+        .attr("stroke", "rgb(248, 248, 248)")
+        .attr("stroke-width", "8")
         //.style("fill", "rgb(119, 119, 119)");
-        .style("fill", "rgb(248, 248, 248)");
+        .attr("fill", "rgb(248, 248, 248)");
 
     cells.append("text")
+        .attr("class", "keyword")
         .attr("x", 5)
         .attr("y", function(d) { return 17; })
         .attr("fill", "rgb(20, 20, 20)")
@@ -54,17 +55,27 @@ KeywordVis.prototype.update = function() {
         if (mouse_y == y && x != mouse_x && Math.abs(x - mouse_x) <= 3)
             return "0.1";
         return "1";
-    }
-    var rect = this.svg.selectAll("rect").data(this.keywordGraph.vertices)
+    };
+
+    var rect = this.svg.selectAll("rect").data(this.keywordGraph.vertices);
+    rect.transition()
+        .duration(100)
+        .ease("linear")
         .attr("fill-opacity", func)
         .attr("stroke-opacity", func);
 
-    text = this.svg.selectAll("text").data(this.keywordGraph.vertices)
-        .attr("fill-opacity", func)
+    text = this.svg.selectAll("text").data(this.keywordGraph.vertices);
+    text.transition()
+        .duration(100)
+        .ease("linear")
         .attr("stroke-opacity", func)
+        .attr("fill-opacity", func)
         .attr("fill", function(d, i) { if (i == mouseover_item) return "rgb(150, 90, 78)"; else return "rgb(20, 20, 20)"; });
 
     var self = this;
-    text.on("mouseover", function(d, i) { mouseover_item = i; self.update(); });
+    text.on("mouseover", function(d, i) {
+        //d3.select(this).transition().duration(1000).attr("fill", "rgb(150, 90, 78)");
+        mouseover_item = i; self.update();
+    });
     this.svg.on("mouseleave", function(d) { mouseover_item = -1; self.update(); });
 }
